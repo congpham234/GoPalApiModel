fe31e9abbd344cb1fe34c37c0c326e3a8d4a1483
Cong pham <>
2024-05-01 01:10:40 -0700

    Add GoPal service name to auto generated code

--- bin/generate-api-models.js
+++ bin/generate-api-models.js
@@ -16,7 +16,7 @@ function main() {
   executeCommand("generate-open-api-doc");
 
   // Run the OpenAPI command to generate client code
-  executeCommand("openapi --input ./openapi/GoPal.openapi.json --output ./generated --client xhr");
+  executeCommand("openapi --name GoPal --input ./openapi/GoPal.openapi.json --output ./generated --client xhr");
 }
 
 main();
